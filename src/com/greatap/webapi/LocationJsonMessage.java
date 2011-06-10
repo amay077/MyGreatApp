daf8f852161b4d31803418264068dba5ba2aa1ed
amay077a <okuokuoku@gmail.com>
2011-06-11 02:41:50 +0900

    JSON からのデコードできた。Token と Expire を覚えておけることを確認した。

---
+++ src/com/greatap/webapi/LocationJsonMessage.java
@@ -0,0 +1,16 @@
+package com.greatap.webapi;
+
+//"street":"\u5927\u5ca9\u753a\u5b57\u5c0f\u5c71\u585a\uff16\uff12\u2010\uff18",
+//"city":"Toyohashi-shi",
+//"country":"Japan",
+//"zip":"441-3131",
+//"latitude":34.72010833,
+//"longitude":137.4468403
+public class LocationJsonMessage {
+	private String street;
+	private String city;
+	private String country;
+	private String zip;
+	private double latitude;
+	private double longitude;
+}
---
+++ src/com/greatap/webapi/SpotJsonMessage.java
@@ -0,0 +1,20 @@
+package com.greatap.webapi;
+
+//"name":"\u30ed\u30fc\u30bd\u30f3\u3000\u8c4a\u6a4b\u4e8c\u5ddd\u5e97",
+//"category":"Local business",
+//"location":
+//{
+//  "street":"\u5927\u5ca9\u753a\u5b57\u5c0f\u5c71\u585a\uff16\uff12\u2010\uff18",
+//  "city":"Toyohashi-shi",
+//  "country":"Japan",
+//  "zip":"441-3131",
+//  "latitude":34.72010833,
+//  "longitude":137.4468403
+//},
+//"id":"189674171081087"
+public class SpotJsonMessage {
+	private String name;
+	private String category;
+	private LocationJsonMessage location;
+	private String id;
+}
